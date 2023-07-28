41654cb70367b9ef5d9ede5ab84c611b9c797d0a
João Victorio <victoriojoao92@gmail.com>
2023-07-28 20:30:33 -0300

    termina ex 5

--- src/components/CardAdicionar/index.jsx
+++ src/components/CardAdicionar/index.jsx
@@ -4,14 +4,21 @@ function CardAdicionar(props) {
   
   const [textoTarefa, setTextoTarefa] = useState("")
   
+  const handleAdicionarTarefa = () => {
+    props.adicionarTarefa(textoTarefa)
+    setTextoTarefa("")
+  }
+  
+  
   return (
     <div>
       <h1>Texto da tarefa: {textoTarefa}</h1>
       <input type="text" 
       value={textoTarefa} 
       onChange={(evento) => setTextoTarefa(evento.target.value)} 
-      placeholder="Adicione uma tarefa" />
-      <button onClick={() => props.adicionarTarefa(textoTarefa)}>Adicionar</button>
+      placeholder="Adicione uma tarefa" 
+      />
+      <button onClick={() => handleAdicionarTarefa()}>Adicionar</button>
     </div>
   )
 }
