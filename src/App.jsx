efb7cc879aa08008070d9cbd4fbcc4b3e1b179f2
João Victorio <victoriojoao92@gmail.com>
2023-07-28 19:18:48 -0300

    termina ex3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import React, { useState } from 'react';
 import './App.css'
 import  CardAdicionar from "./components/CardAdicionar";
 
 function App() {
+
+  const [listaTarefas, setListaTarefas] = useState([
+    {textoTarefa: "tarefa1", 
+    finalizado: false
+    }
+  ]);
+
 
   return (
     <>
