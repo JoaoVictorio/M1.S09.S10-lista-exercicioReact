e811502ae5e7c00ca43b6ddfb2172dc9606d6ea1
João Victorio <victoriojoao92@gmail.com>
2023-07-28 19:13:11 -0300

    termina ex 1

--- src/components/CardAdicionar/index.jsx
+++ src/components/CardAdicionar/index.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 function CardAdicionar() {
+  
+  const [textoTarefa, setTextoTarefa] = useState("")
+  
   return (
-    <div>Card adicionar</div>
+    <div>
+      <h1>Texto da tarefa: {textoTarefa}</h1>
+      <input type="text" 
+      value={textoTarefa} 
+      onChange={(evento) => setTextoTarefa(evento.target.value)} 
+      placeholder="Adicione uma tarefa" />
+      <button onClick={() => {}}>Adicionar</button>
+    </div>
   )
 }
 
