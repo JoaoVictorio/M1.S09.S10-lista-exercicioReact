b8782303a7ee1c9b2284a27542ee8ffd7161f7eb
João Victorio <victoriojoao92@gmail.com>
2023-07-28 19:53:10 -0300

    termina ex4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
 import './App.css'
-import  CardAdicionar from "./components/CardAdicionar";
+import  CardAdicionar from "./components/CardAdicionar"
 
 function App() {
 
-  const [listaTarefas, setListaTarefas] = useState([
-    {textoTarefa: "tarefa1", 
-    finalizado: false
-    }
-  ]);
+  const [listaTarefas, setListaTarefas] = useState([{id: 1, textoTarefa: "tarefa 1", finalizado: false}
+  ])
+  
+  const adicionarTarefa = (texto) => {
+    const novaTarefa = {id: listaTarefas.length + 1, textoTarefa: texto, finalizado: false}
+  
+  
+  setListaTarefas([...listaTarefas, novaTarefa])
+  }
 
 
   return (
     <>
-     <CardAdicionar />
+     <CardAdicionar adicionarTarefa={adicionarTarefa}/>
+     <div>{listaTarefas.map(tarefa => (<span>{tarefa.textoTarefa}</span>))}</div>
     </>
   )
 }
--- src/components/CardAdicionar/index.jsx
+++ src/components/CardAdicionar/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function CardAdicionar() {
+function CardAdicionar(props) {
   
   const [textoTarefa, setTextoTarefa] = useState("")
   
@@ -11,7 +11,7 @@ function CardAdicionar() {
       value={textoTarefa} 
       onChange={(evento) => setTextoTarefa(evento.target.value)} 
       placeholder="Adicione uma tarefa" />
-      <button onClick={() => {}}>Adicionar</button>
+      <button onClick={() => props.adicionarTarefa(textoTarefa)}>Adicionar</button>
     </div>
   )
 }
